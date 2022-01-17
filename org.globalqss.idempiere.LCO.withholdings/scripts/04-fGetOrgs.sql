53035d32de58cedfe2ea99f95ce5511a805102b6
Argenis <argenismrvel@gmail.com>
2022-01-17 15:37:50 -0400

    Update 2Pack 6.3.3

--- org.globalqss.idempiere.LCO.withholdings/scripts/04-fGetOrgs.sql
+++ org.globalqss.idempiere.LCO.withholdings/scripts/04-fGetOrgs.sql
@@ -1,4 +1,3 @@
-
 CREATE OR REPLACE FUNCTION getOrgs(p_AD_Org_ID NUMERIC, p_ExcludeParentOrg VARCHAR)
 	RETURNS TABLE(ad_org_id NUMERIC)
 	LANGUAGE plpgsql
@@ -6,13 +5,8 @@ CREATE OR REPLACE FUNCTION getOrgs(p_AD_Org_ID NUMERIC, p_ExcludeParentOrg VARCH
 AS $$
 DECLARE
 	rr RECORD;
+	v_IsParent VARCHAR := 'N';
 BEGIN
-	
-	IF COALESCE(p_ExcludeParentOrg, 'N') = 'N' THEN
-		ad_org_id := p_AD_Org_ID;
-		RETURN NEXT;
-	END IF;
-	
 	FOR rr IN
 		SELECT
 			ch.ad_org_id
@@ -21,7 +15,13 @@ BEGIN
 		WHERE ao.parent_org_id = p_AD_Org_ID
 		AND ao.isactive = 'Y'
 	LOOP
+		v_IsParent := 'Y';
 		ad_org_id := rr.ad_org_id;
 		RETURN NEXT;
 	END LOOP;
+	
+	IF COALESCE(p_ExcludeParentOrg, 'N') = 'N' OR v_IsParent = 'N' THEN
+		ad_org_id := p_AD_Org_ID;
+		RETURN NEXT;
+	END IF;
 END $$;
